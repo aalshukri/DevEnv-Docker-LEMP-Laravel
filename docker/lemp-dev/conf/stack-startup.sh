7b96e9769bfe7171b69b8b86ea4a1ac1e84412cd
aalshukri <aalshukri@googlemail.com>
2020-11-12 14:54:40 +0000

    Updated with mysql db for Laravel applications.

--- docker/lemp-dev/conf/stack-startup.sh
+++ docker/lemp-dev/conf/stack-startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# start mysql db
 /etc/init.d/mysql start
 
+# setup db
+/setup/setup.sh
+
+# start php
 /etc/init.d/php7.2-fpm start
 
 nginx -g "daemon off;"
---
+++ docker/lemp-dev/setup/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Setup Laravel database
+mysql < /setup/laravel.sql
